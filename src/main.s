2c16cf7498dd783795b27d2ff326e7bc51bd6d12
Gustanol <gualeixos456@gmail.com>
2025-10-09 14:09:07 -0300

    fix: modifying the main file content for GAS assembler (AT&T) syntax and adding a Brainfuck file for tests

--- src/main.s
+++ src/main.s
@@ -1,110 +1,133 @@
-section .data
-    ; initialize a jump table with 256 spaces filled by invalid characters
-    
-    ; dq (define quadword) is used to allocate a space of 8 bytes (an ASCII character)
-    ; therefore, the table will need 2048 bytes of memory (256*8)
-    jmp_table times 256 dq invalid_char
-    filename db "tests/main.bf", 0 ; initialize "filename" variable to store the name of the file it will be open
+.section .data
+    # initialize a jump table with 256 spaces pointing to invalid characters
+    # quad = 8 bytes
+    # therefore, the table will need 2048 bytes of memory (256*8)
+    jmp_table:
+        .rept 256 # repeat the operation 256 times
+            .quad invalid_char
+        .endr
+    filename:
+        .asciz "tests/main.bf" # initialize "filename" variable to store the name of the file it will be open
 
-; instructions section
-section .text
-    global _start
+# instructions section
+.section .text
+    .global _start
 
-; section used to declare a memory segment to a uninitialized static data
-section .bss
-    stat_buf resb 144 ; 144KB
+# section used to declare a memory segment to a uninitialized static data
+.section .bss
+    stat_buf: .space 144 # reserve 144 bytes
 
 _start:
-    ; in here, will define the valid entrances of BF in `jmp_table`
-    
-    ; in each of the following value attribution, we will associate a label pointer
-    ; to quadword (8-byte) value in the correct memory address
-    mov qword [jmp_table + '+'*8], cmd_plus ; '+' = 0x2B (43) * 8 = 0x158 (344) + jmp_table address -> offset representing the character within the table
-    mov qword [jmp_table + '-'*8], cmd_minus
-    mov qword [jmp_table + '>'*8], cmd_right
-    mov qword [jmp_table + '<'*8], cmd_left
+    # in here, it will define the valid entrances of BF in `jmp_table`
+    # in each of the following value attribution, we will associate it to a label pointer
 
-    ; we must to do this 'cause memory is stored continuously
-    ; therefore, to access a value stored within the table, we need to get its value from the address of the table itself
+    # calculate the effective memory of jmp_table and pass it to %rax
+    # the use of %rip register in here is complex and I will explain in the readme.md file of this project
+    leaq jmp_table(%rip), %rax
+    movq $cmd_plus, 0x2B*8(%rax) # 0x2B (+) * 8 = 0x158 + jmp_table address -> offset representing the character within the table
+    movq $cmd_minus, 0x2D*8(%rax) # 0x2D (-)
+    movq $cmd_right, 0x3E*8(%rax) # 0x3E (>)
+    movq $cmd_left, 0x3C*8(%rax) # 0x3C (<)
+    movq $cmd_apos, 0x27*8(%rax) # 0x27 (')
+    movq $cmd_dot, 0x2E*8(%rax) # 0x2E (.)
+    movq $cmd_osqbr, 0x5B*8(%rax) # 0x5B ([)
+    movq $cmd_csqbr, 0x5D*8(%rax) # 0x5D (])
 
-    call load_file ; call label to open the file (filename)
-    call interpret ; call label to interpret the code stored
+    # we must to do this 'cause memory is stored continuously
+    # therefore, to access a value stored within the table, we need to get its value from the address of the table itself
+
+    call load_file # call label to open the file (filename)
+    call interpret # call label to interpret the code stored
+    call end_program # call label to end the program execution
 
 load_file:
-    mov rax, 2; sys_open
-    lea rdi, [filename] ; uses lea (load effective address) to the memory address of `filename` pointer into rdi
-    xor rsi, rsi ; make rsi null to represents zero flag (read only)
-    xor rdx, rdx
-    syscall ; used to call kernel to execute the given syscall
+    movq $2, %rax # sys_open
+    # leaq filename, %rdi # uses lea (load effective address) to load the memory address of `filename` into rdi
+    xor %rsi, %rsi # make rsi null to represents zero flag (read only)
+    xor %rdx, %rdx
+    syscall # used to call kernel to execute the given syscall
 
-    test rax, rax ; make an AND operation to determine if the file was correct opened
+    testq %rax, %rax # AND operation to determine if the file was correct opened
 
-    ; JS (jump if sign) jumps to a specific label if the result of an operation (in this case, test) is negative
-    ; in here, it will jump to the `error` label
+    # JS (jump if sign) jumps to a specific label if the result of an operation (in this case, test) is negative
+    # in here, it will jump to the `error` label
     js error
-    mov r12, rax ; stores file descriptor into `r12` register
+    movq %rax, %r12 # stores file descriptor into `r12` register
 
-    mov rax, 5 ; sys_fstat
-    mov rdi, r12 ; uses the stored file descriptor
-    lea rsi, [stat_buf] ; copies `stat_buf` variable pointer into rsi
+    movq $5, %rax # sys_fstat
+    movq %r12, %rdi # uses the stored file descriptor
+    leaq stat_buf, %rsi # copies `stat_buf` variable pointer into rsi
     syscall
-    test rax, rax
+    testq %rax, %rax
     js error
-    mov r13, [stat_buf + 48] ; r13 will store the file size
-    ; size is stored at 48 offset of the buffer because the struct he populates
 
-    ; C implementation (not all):
-    ; struct stat {
-    ;   dev_t st_dev
-    ;   ino_t st_ino
-    ;   // ...
-    ;   off_t st_size // offset 48
-    ; }
+    movq stat_buf(%rip), %rax
+    movq 48(%rax), %r13 # r13 will store the file size
+    # size is stored at offset 48 of the buffer because of the struct he populates
+
+    # C implementation (not all):
+    # struct stat {
+    #   dev_t st_dev
+    #   ino_t st_ino
+    #   // ...
+    #   off_t st_size // offset 48
+    # }
     
-    ; mmap maps something into the virtual memory of the program
-    ; instead of reading a file and copy data into a buffer, mmap will only pointer to a map
-    mov rax, 9 ; sys_mmap
+    # mmap maps something into the virtual memory of the program
+    # instead of reading a file and copy data into a buffer, mmap will only pointer to a map
+    movq $9, %rax # sys_mmap
 
-    ; in here, we make a xor operation to make RDI (register destination index) null
-    ; therefore, the kernel itself will determine the address to allocate the file (in this case)
+    # in here, we make a xor operation to make RDI (register destination index) null
+    # therefore, the kernel itself will determine the address to allocate the file (in this case)
+    # if we would need to allocate at a specific memory address, we could do this by using:
+    # mov rdi, <memory_address>
+    xor %rdi, %rdi
 
-    ; if we would need to allocate at a specific memory address, we could do this by using:
-    ; mov rdi, <memory_address>
-    xor rdi, rdi
+    # allocates memory to mmap according to file length
+    movq %r13, %rsi # stores the file size into %rsi register
+    movq $1, %rdx
 
-    ; allocates memory to mmap according to file length
-    mov rsi, r13 ; rsi = register source index: represents the local memory (program memory)
-    mov rdx, 1
+    # Protection:
+    # PROT_READ = 1: read
+    # PROT_WRITE = 2: write
+    # PROT_EXEC = 4: execute
+    # ...
 
-    ; Protection:
-    ; PROT_READ = 1: read
-    ; PROT_WRITE = 2: write
-    ; PROT_EXEC = 4: execute
-    ; ...
+    movq $2, %r10
 
-    mov r10, 2
+    # Flags:
+    # MAP_SHARED = 1: changes affect the file (shared)
+    # MAP_PRIVATE = 2: changes does not affect the file, it's kept only in memory (copy-on-write)
+    # MAP_PRIVATE = 0x20: not a file, just memory (malloc)
 
-    ; Flags:
-    ; MAP_SHARED = 1: changes affect the file (shared)
-    ; MAP_PRIVATE = 2: changes does not affect the file, it's kept only in memory (copy-on-write)
-    ; MAP_PRIVATE = 0x20: not a file, just memory (malloc)
-
-    mov r8, r12 ; define file descriptor
-    xor r9, r9 ; offset = 0
+    movq %r12, %r8 # define file descriptor that will be passed in mmap
+    xor %r9, %r9 # offset = 0
     syscall
 
-    test rax, rax
+    testq %rax, %rax
     js error
 
-    mov r14, rax ; mapped code address
+    movq %rax, %r14 # %r14 = mapped code address
 
-    mov rax, 3 ; sys_close
-    mov rdi, r12 ; file descriptor that will be closed
+    movq $3, %rax # sys_close
+    movq %r12, %rdi # file descriptor that will be closed
     syscall
 
 interpret:
 
+clean_mmap:
+    movq $11, %rax # sys_munmap
+    movq %r14, %rdi # mmap address
+    movq %r13, %rsi # mmap size
+    syscall
+
 error:
-    mov rax, 60 ; sys_exit
-    mov rdi, 1 ; exit code (fail)
+    movq $60, %rax # sys_exit
+    movq $1, %rdi # exit code (fail)
+    syscall
+
+end_program:
+    call clean_mmap
+    movq $60, %rax # sys_exit
+    movq $0,  %rdi # exit code (successful)
     syscall
